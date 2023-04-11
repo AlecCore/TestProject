b1ca829ed7e31f0e1287300bcc554ed4edd29200
Данилюк Альоша <alocha2001@gmail.com>
2023-04-11 17:53:16 +0200

    Добавьте файлы проекта.

---
+++ TestProject/PasswordData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class PasswordData
+    {
+        string SearchChar { get; set; }
+        string Password { get; set; }
+        int MinLenght { get; set; }
+        int MaxLenght { get; set; }
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace TestProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(Directory.GetCurrentDirectory() + @"\password.txt");
+            Console.WriteLine(lines);
+        }
+    }
+}
